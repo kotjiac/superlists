a9e733fea9632c3496ff563eaea95ded74c08d92
RonaldoTeixeira <ronaldo_t@hotmail.com>
2024-07-15 15:16:36 -0300

    ajustes testes unitários

--- lists/tests.py
+++ lists/tests.py
@@ -21,13 +21,13 @@ class HomePageTest(TestCase):
 class NewListTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
-		self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.client.post('/lists/new', data={'item_text': 'A new list item', 'item_priority': 'prioridade+baixa'})
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
 	def test_redirects_after_POST(self):
-		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item', 'item_priority': 'prioridade+baixa'})
 		new_list = List.objects.first()
 		self.assertRedirects(response, f'/lists/{new_list.id}/')
 
@@ -39,7 +39,7 @@ class NewItemTets(TestCase):
 
 		self.client.post(
 			f'/lists/{correct_list.id}/add_item',
-			data={'item_text': 'A new item for an existing list'}
+			data={'item_text': 'A new item for an existing list', 'item_priority': 'prioridade+baixa'}
 		)
 
 		self.assertEqual(Item.objects.count(), 1)
@@ -53,7 +53,7 @@ class NewItemTets(TestCase):
 
 		response = self.client.post(
 			f'/lists/{correct_list.id}/add_item',
-			data={'item_text': 'A new item for an existing list'}
+			data={'item_text': 'A new item for an existing list', 'item_priority': 'prioridade+alta'}
 		)
 
 		self.assertRedirects(response, f'/lists/{correct_list.id}/')
@@ -69,19 +69,19 @@ class ListViewTest(TestCase):
 
 	def test_displays_only_items_for_that_list(self):
 		correct_list = List.objects.create()
-		Item.objects.create(text='itemey 1', list=correct_list)
-		Item.objects.create(text='itemey 2', list=correct_list)
+		Item.objects.create(text='itemey 1', priority='prioridade+baixa', list=correct_list)
+		Item.objects.create(text='itemey 2', priority='prioridade+alta', list=correct_list)
 
 		other_list = List.objects.create()
-		Item.objects.create(text='other list item 1', list=other_list)
-		Item.objects.create(text='other list item 2', list=other_list)
+		Item.objects.create(text='other list item 1', priority='prioridade+média', list=other_list)
+		Item.objects.create(text='other list item 2', priority='prioridade+alta', list=other_list)
 
 		response = self.client.get(f'/lists/{correct_list.id}/')
 
-		self.assertContains(response, 'itemey 1')
-		self.assertContains(response, 'itemey 2')
-		self.assertNotContains(response, 'other list item 1')
-		self.assertNotContains(response, 'other list item 2')
+		self.assertContains(response, 'itemey 1 - prioridade+baixa')
+		self.assertContains(response, 'itemey 2 - prioridade+alta')
+		self.assertNotContains(response, 'other list item 1 - prioridade+média')
+		self.assertNotContains(response, 'other list item 2 - prioridade+alta')
 
 	def test_passes_correct_list_to_template(self):
 		other_list = List.objects.create()
